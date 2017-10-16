d59b4dcdba1a6fe092fc8ba76e4048a0d34dc5cf
Andre Staltz <andre@staltz.com>
2017-10-16 22:26:25 +0300

    Add support for node.js stdout to show in Android logcat

--- android/src/main/java/com/staltz/reactnativenode/RNNodeThread.java
+++ android/src/main/java/com/staltz/reactnativenode/RNNodeThread.java
@@ -2,10 +2,14 @@ package com.staltz.reactnativenode;
 
 import android.util.Log;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class RNNodeThread extends Thread {
     private static final String TAG = "RNNodeThread";
+    private static final String NODETAG = "nodejs";
 
     private boolean _running;
     private Process _process;
@@ -25,23 +29,42 @@ public class RNNodeThread extends Thread {
         }
     }
 
+    static class Logger implements Runnable {
+        private BufferedReader reader;
+        private final boolean isError;
+
+        public Logger(InputStream rS, boolean isError) throws IOException {
+            this.reader = new BufferedReader(new InputStreamReader(rS));
+            this.isError = isError;
+        }
+
+        @Override
+        public void run() {
+            try {
+                String line;
+                while ((line = this.reader.readLine()) != null) {
+                    if (this.isError) {
+                        Log.e(NODETAG, line);
+                    } else {
+                        Log.v(NODETAG, line);
+                    }
+                }
+            } catch (IOException e) {
+                Log.e(TAG + ".Logger", e.toString());
+            }
+        }
+    }
+
     @Override
     public void run() {
         try {
             _process = _processBuilder.start();
             Log.i(TAG, "Node.js process is running...");
-            // Report output
-            Scanner s1 = new Scanner(_process.getInputStream()).useDelimiter("\\A");
-            String output = s1.hasNext() ? s1.next() : "";
-            if (output != null && output.length() > 0) {
-                Log.v(TAG, output);
-            }
-            // Report errors
-            Scanner s = new Scanner(_process.getErrorStream()).useDelimiter("\\A");
-            String errors = s.hasNext() ? s.next() : "";
-            if (errors != null && errors.length() > 0) {
-                Log.e(TAG, errors);
-            }
+            final Thread stdoutThread = new Thread(new Logger(_process.getInputStream(), false));
+            final Thread stderrThread = new Thread(new Logger(_process.getErrorStream(), true));
+            stdoutThread.run();
+            stderrThread.run();
+            _process.waitFor();
         } catch (Exception e) {
             // probably interrupted
             Log.e(TAG, "Error running Node.js process:", e);
